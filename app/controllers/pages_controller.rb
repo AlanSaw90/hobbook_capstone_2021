dc53b847458bf597140e852d9c479adb1c9f2800
Alan Saw Kah Wai <alanjiawei90@gmail.com>
2021-08-28 20:19:43 +0800

    signup-login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :aunthenticate_user!
+  skip_before_action :authenticate_user!
 
   def homepage
     @welcome ="Welcome to HobBook"
