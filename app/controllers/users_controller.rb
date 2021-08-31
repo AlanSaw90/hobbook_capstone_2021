6f877011c16a806723417f4a28f4cbdf0f311be7
Alan Saw Kah Wai <alanjiawei90@gmail.com>
2021-09-01 00:54:29 +0800

    Profile-update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,22 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+  if @user.save
+    redirect_to user_path(@user), notice: 'Your profile was successfully updated'
+  else
+    render :edit
   end
 end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :birth_date, :avatar)
+    end
+  end
