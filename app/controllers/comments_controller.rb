dad559f06c111a6948c3f33c0ca908bb70433449
Alan Saw Kah Wai <alanjiawei90@gmail.com>
2021-09-04 13:15:20 +0800

    comment_params_id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,17 @@
 class CommentsController < ApplicationController
     def create
-
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.create(comment_params.merge(user_id: current_user.id))
+    
+        if @comment.save 
+            redirect_to post_path(@post), notice: 'Comment was successfully created'
+        else 
+            redirect_to post_path(@post), notice: 'There was an error'            
     end   
-end    
+end  
+
+    private
+        def comment_params
+            params.require(:comment).permit(:content)
+        end
+    end        
